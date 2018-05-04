5003e3129dc3cf539b2828b52ffe343b5c8d17f0
Nimor111 <georgi.bojinov@hotmail.com>
2018-05-04 14:51:44 +0300

    Ignore local firebase config

--- simple-blogging/src/firebase.js
+++ simple-blogging/src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-const config = {
-  apiKey: "AIzaSyCMKUCTz3b6-NoiCw89TbDmNHJ3sdW_RMU",
-  authDomain: "simple-blogging-df6ab.firebaseapp.com",
-  databaseURL: "https://simple-blogging-df6ab.firebaseio.com",
-  projectId: "simple-blogging-df6ab",
-  storageBucket: "simple-blogging-df6ab.appspot.com",
-  messagingSenderId: "777633398262"
-};
+import config from "./config.json";
 
 firebase.initializeApp(config);
 
